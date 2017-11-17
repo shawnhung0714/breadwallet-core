75e27fbd67b3afda7fcb87c3d4bbd91091845b90
Shawn Hung <shawnhung0714@gmail.com>
2017-11-18 07:01:56 +0800

    Fix transaction param.

--- BRMerkleBlock.c
+++ BRMerkleBlock.c
@@ -321,12 +321,12 @@ int BRMerkleBlockVerifyDifficulty(const BRMerkleBlock *block, const BRMerkleBloc
     if (! previous || !UInt256Eq(block->prevBlock, previous->blockHash) || block->height != previous->height + 1) r = 0;
     if (r && (block->height % BLOCK_DIFFICULTY_INTERVAL) == 0 && transitionTime == 0) r = 0;
     
-    if (block->height >= 478559) return r;
-    
 #if BITCOIN_TESTNET
     // TODO: implement testnet difficulty rule check
     return r; // don't worry about difficulty on testnet for now
 #endif
+    
+    if (block->height >= 478559) return r;
     
     if (r && (block->height % BLOCK_DIFFICULTY_INTERVAL) == 0) {
         // target is in "compact" format, where the most significant byte is the size of resulting value in bytes, next
